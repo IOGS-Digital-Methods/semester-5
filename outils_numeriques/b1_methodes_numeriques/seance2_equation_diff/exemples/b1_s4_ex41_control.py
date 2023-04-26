32f78b347f57c1599c85ba91302a8255a6d6db71
Julien Villemejane <julien.villemejane@institutoptique.fr>
2023-04-26 21:08:33 +0200

    Update Bloc 1

---
+++ outils_numeriques/b1_methodes_numeriques/seance2_equation_diff/exemples/b1_s4_ex41_control.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Module Outils Numériques / Semestre 5 / Institut d'Optique
+
+Approche Système / découverte de la bibliothèque control
+
+Created on 26/Apr/2023
+
+@author: LEnsE / IOGS / Palaiseau
+@author: Julien Villemejane
+"""
+
+import numpy as np
+from matplotlib import pyplot as plt
+from IPython.display import *
+
+#%% Définition du système
+import control as ct
+
+num = np.array([1, -1])
+den = np.array([1, -2, 1])
+
+tf_sys = ct.tf(num, den)
+
+display(tf_sys)
+
+#%% Diagramme de Bode
+
+omega = np.logspace(-2, 2, 101)
+
+ct.bode_plot( sys , omega=w)
