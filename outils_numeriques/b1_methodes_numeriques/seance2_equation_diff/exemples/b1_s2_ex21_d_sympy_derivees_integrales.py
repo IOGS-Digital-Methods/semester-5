dd507490dd3cf37909c998eb1aab70cd09cd9607
Julien Villemejane <julien.villemejane@institutoptique.fr>
2023-05-12 15:03:07 +0200

    Update b1_s2_ex21_d_sympy_derivees_integrales.py

--- outils_numeriques/b1_methodes_numeriques/seance2_equation_diff/exemples/b1_s2_ex21_d_sympy_derivees_integrales.py
+++ outils_numeriques/b1_methodes_numeriques/seance2_equation_diff/exemples/b1_s2_ex21_d_sympy_derivees_integrales.py
@@ -3,7 +3,7 @@
 """
 Module Outils Numériques / Semestre 5 / Institut d'Optique
 
-Calcul symbolique SymPy
+Calcul symbolique SymPy / Intégrales et Dérivées
 
 Created on 15/Apr/2023
 
@@ -15,47 +15,29 @@ import math
 import sympy
 from IPython.display import *
 
-#%% Premiers essais
-print(math.sqrt(9))
-print(math.sqrt(8))
+#%% Derivees
 
-display(sympy.sqrt(9))
-print(sympy.sqrt(8))
-display(sympy.sqrt(8))
+# Exemple 1
+f = x**2 + y
+display(f)
 
-# Quelle différence entre print et display ?
-# Quelle différence entre math.sqrt(8) et sympy.sqrt(8)
+dfx = sympy.diff(f, x)
+display(dfx)
 
-#%% Nombres rationnels
-m = 3/2
-print(m)
+dfy = sympy.diff(f, y)
+display(dfy)
 
-k = sympy.Rational(3,2)
-print(k)
-display(k)
+# Exemple 2
+k = sympy.Function('k')
+l = sympy.Function('l')
+k = k(x)
+l = l(x + k)
+k, l
 
-#%% Expressions
-x, y = sympy.symbols('x y')
-expr = x**2 - 4 * x + 5
-display(expr)
+dfkl = sympy.diff(l, x)
+display(dfkl)
 
-val = expr.subs(x, 1)
-print(val)
 
-# Que contient expr ?
-# A quoi sert la fonction subs ?
-
-#%% Développer et factoriser
-poly = x*expr
-display(poly)
-
-poly_exp = sympy.expand(poly)
-display(poly_exp)
-
-poly_fact = sympy.factor(poly_exp)
-display(poly_fact)
-
-# Que contiennent les expressions poly_exp et poly_fact ?
-# A quoi servent les fonctions expand et factor ?
-
-#%% Fonctions
+#%% Integrales
+inte_f = sympy.integrate(f, x)
+display(inte_f)
