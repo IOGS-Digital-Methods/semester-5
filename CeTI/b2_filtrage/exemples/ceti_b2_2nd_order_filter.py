7ea0b79332338e304a2d50a090a57df7fcde2298
Julien Villemejane <julien.villemejane@institutoptique.fr>
2023-07-10 09:16:51 +0200

    Reorganisation CeTI

---
+++ CeTI/b2_filtrage/exemples/ceti_b2_2nd_order_filter.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Module Conception Electronique / Semestre 5 / Institut d'Optique
+
+Filtrage actif / Modélisation et simulation
+
+Created on 10/Jul/2023
+
+Author : Julien Villemejane / LEnsE / IOGS
+"""
