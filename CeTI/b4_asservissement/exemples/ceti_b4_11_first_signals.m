5639be411286ba30d7b3d0e5d9e141fb7655f2e7
Julien Villemejane <julien.villemejane@institutoptique.fr>
2023-07-12 14:41:23 +0200

    Update ceti_b4_11_first_signals.m

--- CeTI/b4_asservissement/exemples/ceti_b4_11_first_signals.m
+++ CeTI/b4_asservissement/exemples/ceti_b4_11_first_signals.m
@@ -22,23 +22,23 @@ vs = A * sin(2*pi*f0*vt);
 %% Affichage
 figure()
 plot(vt, vs)
-str = sprintf('Sine Wave - at f0 = %.2f / A = %.2f', f0, A)
-title(str)
+str = sprintf('Sine Wave - at f0 = %.2f / A = %.2f', f0, A);
+title(str);
 
 %% Transformée de Fourier
 fft_vs = fft(vs);
 fft_vs_shift = fftshift(fft_vs);
-vt_size = length(vt)
-n_padding = 8
-fft_vs_padding = fft(vs, n_padding*vt_size)
-fft_vs_padding_shift = fftshift(fft_vs_padding)
+vt_size = length(vt);
+n_padding = 8;
+fft_vs_padding = fft(vs, n_padding*vt_size);
+fft_vs_padding_shift = fftshift(fft_vs_padding);
 
 % Axe des fréquences
-Fe = 1 / (vt(2)-vt(1))
-freq_fft = linspace(0, Fe-Fe/vt_size, vt_size)
-freq_fft_shift = fftshift(freq_fft)
-freq_fft_padding = linspace(0, Fe-Fe/(n_padding*vt_size), n_padding*vt_size)
-freq_fft_padding_shift = fftshift(freq_fft_padding)
+Fe = 1 / (vt(2)-vt(1));
+freq_fft = linspace(0, Fe-Fe/vt_size, vt_size);
+freq_fft_shift = fftshift(freq_fft);
+freq_fft_padding = linspace(0, Fe-Fe/(n_padding*vt_size), n_padding*vt_size);
+freq_fft_padding_shift = fftshift(freq_fft_padding);
 
 % Figure
 figure()
