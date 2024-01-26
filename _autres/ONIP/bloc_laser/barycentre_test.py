9c4d1609abea52acf1e1dcd18384d6ae2f23b4b6
Julien Villemejane <julien.villemejane@institutoptique.fr>
2024-01-26 13:57:53 +0100

    Update Barycentre

--- _autres/ONIP/bloc_laser/barycentre_test.py
+++ _autres/ONIP/bloc_laser/barycentre_test.py
@@ -12,13 +12,38 @@ def barycentre(tab2D):
 	X = np.linspace(0,length-1,length)
 	XX,YY= np.meshgrid(X,Y)
 	sum_tab = np.sum(tab2D)
-	x_bary = np.sum(XX * tab2D) // sum_tab
-	y_bary = np.sum(YY * tab2D) // sum_tab
-	return x_bary, y_bary
+	x_bary = np.floor(np.sum(XX * tab2D) // sum_tab)
+	y_bary = np.floor(np.sum(YY * tab2D) // sum_tab)
+	return int(x_bary), int(y_bary)
 
 
 if __name__ == '__main__':
-	image = Image.open('Profil1.tif')
+	image_tif = Image.open('Profil1.tif')
+	image = image_tif.convert("L")
+	image = np.asarray(image)
 	plt.figure()
 	plt.imshow(image)
+	x_b, y_b = barycentre(image)
+	plt.axvline(x_b)
+	plt.axhline(y_b)
+
+	# slice
+	slice_image = image[x_b, :]
+	plt.figure()
+	plt.plot(slice_image)
+	
+	# Image PNG Brute
+	image_png = Image.open('Profil1_b.png')
+	image = image_png.convert("L")
+	image = np.asarray(image)
+	plt.figure()
+	plt.imshow(image)
+	x_b, y_b = barycentre(image)
+	plt.axvline(x_b)
+	plt.axhline(y_b)
+	
+	# slice
+	slice_image = image[x_b, :]
+	plt.figure()
+	plt.plot(slice_image)
 	plt.show()
