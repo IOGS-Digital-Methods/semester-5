28aa7ee7136dfc12dde67a3ed29143bf8b21701d
Julien Villemejane <julien.villemejane@institutoptique.fr>
2024-01-26 17:50:31 +0100

    Update barycentre

--- _autres/ONIP/bloc_laser/barycentre_test.py
+++ _autres/ONIP/bloc_laser/barycentre_test.py
@@ -15,35 +15,42 @@ def barycentre(tab2D):
 	x_bary = np.floor(np.sum(XX * tab2D) // sum_tab)
 	y_bary = np.floor(np.sum(YY * tab2D) // sum_tab)
 	return int(x_bary), int(y_bary)
+    
+def find_max(tab2D):
+    return np.where(tab2D == tab2D.max())
 
 
 if __name__ == '__main__':
-	image_tif = Image.open('Profil1.tif')
-	image = image_tif.convert("L")
-	image = np.asarray(image)
-	plt.figure()
-	plt.imshow(image)
-	x_b, y_b = barycentre(image)
-	plt.axvline(x_b)
-	plt.axhline(y_b)
+	# Airy
+    image_png = Image.open('airy_2mm.bmp')
+    image = image_png.convert("L")
+    image = np.asarray(image)
+    plt.figure()
+    plt.imshow(np.log(image+0.1))
+    
+    # Find max and crop
+    i_max_x, i_max_y = find_max(image)
+    print(i_max_x.shape)
+    print(i_max_x)
+    
+    """
+    pas = 200
+    image_crop = image[i_max_x-pas:i_max_x+pas, i_max_y-pas:i_max_y+pas]
+    
+    plt.figure()
+    plt.imshow(np.log(image_crop+0.1))
+    
+    x_b, y_b = barycentre(image_crop)
 
-	# slice
-	slice_image = image[x_b, :]
-	plt.figure()
-	plt.plot(slice_image)
-	
-	# Image PNG Brute
-	image_png = Image.open('Profil1_b.png')
-	image = image_png.convert("L")
-	image = np.asarray(image)
-	plt.figure()
-	plt.imshow(image)
-	x_b, y_b = barycentre(image)
-	plt.axvline(x_b)
-	plt.axhline(y_b)
+    plt.figure()
+    plt.imshow(np.log(image_crop+0.1))
+    plt.axvline(x_b)
+    plt.axhline(y_b)
 	
 	# slice
-	slice_image = image[x_b, :]
-	plt.figure()
-	plt.plot(slice_image)
-	plt.show()
+    slice_image = image_crop[x_b, :]
+    plt.figure()
+    plt.plot(np.log(slice_image))
+    
+    plt.show()
+    """
