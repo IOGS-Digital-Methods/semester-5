0683bb6696ac217b089a85b443fe40939166233b
Julien Villemejane <julien.villemejane@institutoptique.fr>
2024-01-26 21:07:21 +0100

    Update barycentre_test.py

--- _autres/ONIP/bloc_laser/barycentre_test.py
+++ _autres/ONIP/bloc_laser/barycentre_test.py
@@ -17,10 +17,26 @@ def barycentre(tab2D):
 	return int(x_bary), int(y_bary)
     
 def find_max(tab2D):
-    return np.where(tab2D == tab2D.max())
+    max_ind = np.argmax(tab2D)
+    print(max_ind)
+    width = tab2D.shape[0]
+    length = tab2D.shape[1]
+    ind_x = int(max_ind / length)
+    ind_y = int(max_ind % width)
+    return ind_x, ind_y
 
 
 if __name__ == '__main__':
+    """
+    # Test image
+    image = np.zeros((100, 200))
+    image[52, 69] = 100
+    
+    x, y = find_max(image)
+    print(f'x = {x} / y = {y}')
+    """
+
+    
 	# Airy
     image_png = Image.open('airy_2mm.bmp')
     image = image_png.convert("L")
@@ -28,18 +44,15 @@ if __name__ == '__main__':
     plt.figure()
     plt.imshow(np.log(image+0.1))
     
-    # Find max and crop
+    # Find max 
     i_max_x, i_max_y = find_max(image)
-    print(i_max_x.shape)
-    print(i_max_x)
+    plt.axvline(i_max_y)
+    plt.axhline(i_max_x)
     
-    """
+    # Crop    
     pas = 200
     image_crop = image[i_max_x-pas:i_max_x+pas, i_max_y-pas:i_max_y+pas]
-    
-    plt.figure()
-    plt.imshow(np.log(image_crop+0.1))
-    
+        
     x_b, y_b = barycentre(image_crop)
 
     plt.figure()
@@ -50,7 +63,6 @@ if __name__ == '__main__':
 	# slice
     slice_image = image_crop[x_b, :]
     plt.figure()
-    plt.plot(np.log(slice_image))
+    plt.plot(np.log(slice_image+0.1))
     
     plt.show()
-    """
